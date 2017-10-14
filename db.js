09ee27a483009370e7071c7cc293fa719c64aea1
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-14 14:36:47 +0700

    CRUD

--- db.js
+++ db.js
@@ -12,5 +12,29 @@ const Word = mongoose.model('words', WordSchema);
 
 mongoose.connect('mongodb://localhost/rn1609', { useMongoClient: true });
 
-Word.find({})
-.then(words => console.log(words));
+// create
+
+// const word = new Word({ en: 'code', vn: 'ma nguon' });
+// word.save()
+// .then(word => console.log(word))
+// .catch(err => console.log(err));
+
+// update
+
+// Word.findByIdAndUpdate('59e1b51b851f79de0df357a4', { vn: 'Buoi Sang' })
+// .then(x => console.log(x))
+// .catch(err => console.log(err));
+
+// read
+
+// Word.find({ vn: 'ma nguon' })
+// .then(words => console.log(words))
+// .catch(err => console.log(err));
+
+// Word.findById('59e1b51b851f79de0df357a4')
+// .then(word => console.log(word));
+
+// DELETE
+
+// Word.findByIdAndRemove('59e1bac78345605390aa5017')
+// .then(x => console.log(x));
